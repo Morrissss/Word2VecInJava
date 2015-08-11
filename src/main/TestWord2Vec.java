190c273dce1aee97ba410970a77c9b02503a2410
Morris <runchao.mao@dianping.com>
2015-08-11 14:35:14 +0800

    fix

--- src/main/TestWord2Vec.java
+++ src/main/TestWord2Vec.java
@@ -2,21 +2,21 @@ package main;
 
 import training.VectorModel;
 import training.Word2Vec;
+import training.WordScore;
 
-import java.util.Collections;
-import java.util.Set;
+import java.util.List;
 
 public class TestWord2Vec {
 
     public static void readByJava(String textFilePath, String tmpFilePath, String modelFilePath) throws Exception {
 
         Word2Vec wv = new Word2Vec.Factory()
-                .setMethod(Word2Vec.Method.SKIP_GRAM)
-                .setFreqThresold(5)
-                .setNumOfThread(10)
-                .setWindow(8)
-                .setSample(1e-4)
-                .setVectorSize(200).build();
+		                .setMethod(Word2Vec.Method.SKIP_GRAM)
+		                .setFreqThresold(5)
+		                .setNumOfThread(10)
+		                .setWindow(8)
+		                .setSample(1e-4)
+		                .setVectorSize(200).build();
 
         wv.training(textFilePath, tmpFilePath);
         wv.saveModel(modelFilePath);
@@ -24,10 +24,8 @@ public class TestWord2Vec {
 
     public static void testVector(String modelFilePath) throws Exception {
         VectorModel vm = VectorModel.loadFromFile(modelFilePath);
-        Set<VectorModel.WordScore> result1 = Collections.emptySet();
-
-        result1 = vm.nearestTopN("亲");
-        for (VectorModel.WordScore we : result1){
+        List<WordScore> result = vm.nearestTopN("亲", 20);
+        for (WordScore we : result){
             System.out.println(we.name + " :\t" + we.score);
         }
     }
